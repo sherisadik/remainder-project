4c4666af3595c0d0768511abf414b2301b2d6872
sherisadik <sherisadik123@gmail.com>
2021-07-29 11:11:06 +0530

    first

---
+++ remainderproject/remainderapp/urls.py
@@ -0,0 +1,23 @@
+from .import views
+from django .urls import path,include
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.add,name='list'),
+
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+
+    path('update/<int:id>/',views.update,name='update'),
+
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+
+    path('cbvdetail/<int:pk>/',views.Taskdetailview.as_view(),name='cbvdetail'),
+
+    path('cbvupdate/<int:pk>/',views.Taskupdateview.as_view(),name='cbvupdate'),
+
+    path('cbvdelete/<int:pk>/',views.Taskdeleteview.as_view(),name='cbvdelete'),
+
+
+
+
+]
